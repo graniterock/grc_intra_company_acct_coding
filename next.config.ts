a7a9fe379c6745e1f7a3e389790e675449d0ff49
4xsurfer <64271731+4xsurfer@users.noreply.github.com>
2025-11-11 09:11:27 -0800

    Refactor next.config.ts to use NextConfig type

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
-// next.config.ts
-const nextConfig = {
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
+
 export default nextConfig;
