680a839dde87bfd7edfdb00488c7e47a1511a20f
Shawn Horton <shorton@graniterock.com>
2025-11-17 14:44:23 -0800

    Use ExportStatus not equal E

--- src/lib/sql-server.ts
+++ src/lib/sql-server.ts
@@ -150,7 +150,7 @@ const TICKETS_QUERY = `
         UnitPrice,
         ExportStatus
     FROM Tkhist1
-    WHERE ExportStatus = 'X'
+    WHERE ExportStatus <> 'E'
       AND VoidStatus <> 'V'
 ),
 other_src AS (
